b1c7bfc4af332c1c0106ee6b439cd8592f1d5f86
yashv7982 <yashv7982@gmail.com>
2022-10-18 21:48:38 +0530

    3rd commit

---
+++ node.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static(__dirname+'/public'))
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+  
+});
+
+
+router.get('/services',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/Services.html'));
+});
+router.get('/contact-us',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/contact-us.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
